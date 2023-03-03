6bb4b77d2a08b5dafef5bdbd4498cf159e819b60
Verkhovskiy <artemverhovskiy@itcenterifmo.onmicrosoft.com>
2023-03-03 19:39:37 +0200

    Task 3 completed

---
+++ src/Task03/Tree.java
@@ -0,0 +1,44 @@
+package Task03;
+
+public class Tree {
+    private Integer age;
+    private Boolean isAlive;
+    private String name;
+
+    public Tree(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public Tree(Integer age, Boolean isAlive, String name) {
+        this.age = age;
+        this.isAlive = isAlive;
+        this.name = name;
+    }
+    public Tree() {
+        System.out.println("Пустой конструктор без параметров сработал");
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(Boolean alive) {
+        isAlive = alive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
